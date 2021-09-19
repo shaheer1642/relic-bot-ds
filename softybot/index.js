6d7842d290bdc7e6c3d4e73edacc3bf03a180128
MrSofty123 <shaheer1642@gmail.com>
2021-09-19 13:30:40 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -406,7 +406,7 @@ client.login(config.token).catch(err => console.log(err));
 
 //------------Command functions---------------
 function uptime(message,args) {
-    message.channel.send({content: "(Cloud [Limited functionality]) Current uptime: " + msToTime(new Date().getTime() - tickcount)}).catch(err => console.log(err));
+    message.channel.send({content: "( [Limited functionality]) Current uptime: " + msToTime(new Date().getTime() - tickcount)}).catch(err => console.log(err));
     message.react("✅")
     return
 }
