19517b9ca934aee257c1d4f157dd9980b4a86b19
MrSofty123 <shaheer1642@gmail.com>
2021-09-28 22:26:01 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -3070,12 +3070,12 @@ async function trading_bot(message,args,command) {
         return
     }
     if (price > (avg_price*1.2)) {
-        message.channel.send(`⚠️ Your price is a lot greater than the average price of ${avg_price} for ${item_name} ⚠️\nTry lowering it`).then(msg => setTimeout(() => msg.delete(), 5000)).catch(err => console.log(err));
+        message.channel.send(`⚠️ Your price is a lot **greater than** the average price of **${avg_price}** for **${item_name}** ⚠️\nTry lowering it`).then(msg => setTimeout(() => msg.delete(), 5000)).catch(err => console.log(err));
         setTimeout(() => message.delete().catch(err => console.log(err)), 5000)
         return
     }
     else if (price < (avg_price*0.8)) {
-        message.channel.send(`⚠️ Your price is a lot lower than the average price of ${avg_price} for ${item_name} ⚠️\nTry increaing it`).then(msg => setTimeout(() => msg.delete(), 5000)).catch(err => console.log(err));
+        message.channel.send(`⚠️ Your price is a lot **lower than** the average price of **${avg_price}** for **${item_name}** ⚠️\nTry increaing it`).then(msg => setTimeout(() => msg.delete(), 5000)).catch(err => console.log(err));
         setTimeout(() => message.delete().catch(err => console.log(err)), 5000)
         return
     }
