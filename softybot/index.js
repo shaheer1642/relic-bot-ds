af7b77a6aaf34ba6750b144d2b3e281bc7d86212
MrSofty123 <shaheer1642@gmail.com>
2021-09-28 22:27:49 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -3075,7 +3075,7 @@ async function trading_bot(message,args,command) {
         return
     }
     else if (price < (avg_price*0.8)) {
-        message.channel.send(`⚠️ Your price is a lot **lower than** the average price of **${avg_price}** for **${item_name}** ⚠️\nTry increaing it`).then(msg => setTimeout(() => msg.delete(), 5000)).catch(err => console.log(err));
+        message.channel.send(`⚠️ Your price is a lot **lower than** the average price of **${avg_price}** for **${item_name}** ⚠️\nTry increasing it`).then(msg => setTimeout(() => msg.delete(), 5000)).catch(err => console.log(err));
         setTimeout(() => message.delete().catch(err => console.log(err)), 5000)
         return
     }
