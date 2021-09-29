e9c47587f71bfffaabdd7181c99a132402d039d6
MrSofty123 <shaheer1642@gmail.com>
2021-09-30 00:23:10 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -3733,6 +3733,53 @@ async function trading_bot_user_orders(message,args) {
     })
     if (!status)
         return
+    var postdata = {
+        "content": "Mason is looking for new arena partners. What classes do you play?",
+        "components": [
+            {
+                "type": 1,
+                "components": [
+                    {
+                        "type": 3,
+                        "custom_id": "class_select_1",
+                        "options":[
+                            {
+                                "label": "Rogue",
+                                "value": "rogue",
+                                "description": "Sneak n stab",
+                                "emoji": {
+                                    "name": "rogue",
+                                    "id": "625891304148303894"
+                                }
+                            },
+                            {
+                                "label": "Mage",
+                                "value": "mage",
+                                "description": "Turn 'em into a sheep",
+                                "emoji": {
+                                    "name": "mage",
+                                    "id": "625891304081063986"
+                                }
+                            },
+                            {
+                                "label": "Priest",
+                                "value": "priest",
+                                "description": "You get heals when I'm done doing damage",
+                                "emoji": {
+                                    "name": "priest",
+                                    "id": "625891303795982337"
+                                }
+                            }
+                        ],
+                        "placeholder": "Choose a class",
+                        "min_values": 1,
+                        "max_values": 3
+                    }
+                ]
+            }
+        ]
+    }
+    message.channel.send(postdata).catch(err => console.log(err))
 }
 
 async function trading_bot_registeration(message,ingame_name) {
