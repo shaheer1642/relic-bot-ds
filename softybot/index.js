d7444ba95b38bea2764d2104d8f08da12800cce0
MrSofty123 <shaheer1642@gmail.com>
2021-09-29 21:12:41 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -124,7 +124,7 @@ client.on('messageCreate', async message => {
             const command = args.shift()
     
             if (command == 'wts' || command == 'wtb') {
-                if (message.author.id != '253525146923433984' && message.author.id != '892087497998348349') {
+                if (message.author.id != '253525146923433984' && message.author.id != '892087497998348349' && message.author.id != '212952630350184449') {
                     message.channel.send('🛑 Trading is disabled right now. Please try again later <:ItsFreeRealEstate:892141191301328896>').then(msg => setTimeout(() => msg.delete(), 5000)).catch(err => console.log(err))
                     setTimeout(() => message.delete().catch(err => console.log(err)), 5000)
                     return
