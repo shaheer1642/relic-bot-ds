fd31995f2cb667e33686438fb3fd3eeba3e91703
MrSofty123 <shaheer1642@gmail.com>
2021-10-02 03:26:06 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -254,7 +254,7 @@ client.on('messageCreate', async message => {
                         .then(res => {
                             console.log(`Setting auto-closure for username = ${message.author.username} AND item_name = '${item_name}' AND order_type = '${order_type}`)
                             setTimeout(async () => {
-                                var status = await db.query(`SELECT * FROM WHERE discord_id = ${originMessage.author.id} AND item_id = '${item_id}' AND order_type = '${order_type}'`)
+                                var status = await db.query(`SELECT * FROM users_orders WHERE discord_id = ${originMessage.author.id} AND item_id = '${item_id}' AND order_type = '${order_type}'`)
                                 .then(res => {
                                     if (res.rows.length == 0)
                                         return false
@@ -3859,7 +3859,7 @@ async function trading_bot(message,args,command) {
     const func = await trading_bot_orders_update(originMessage,item_id,item_url,item_name,1)
     .then(res => {
         setTimeout(async () => {
-            var status = await db.query(`SELECT * FROM WHERE discord_id = ${originMessage.author.id} AND item_id = '${item_id}' AND order_type = '${command}'`)
+            var status = await db.query(`SELECT * FROM WHERE users_orders discord_id = ${originMessage.author.id} AND item_id = '${item_id}' AND order_type = '${command}'`)
             .then(res => {
                 if (res.rows.length == 0)
                     return false
