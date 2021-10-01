ea8e66895c9ff4918418b227218a62704c4ecb42
MrSofty123 <shaheer1642@gmail.com>
2021-10-01 13:09:49 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -154,7 +154,7 @@ client.on('messageCreate', async message => {
                 */
                 const func = await trading_bot(message,args,command).then(() => console.log(`executed request ${commandsArr[commandsArrIndex]} for user ${message.author.username}`)).catch(err => console.log(`Some error occured updating order`))
                 console.log(`commandsArrIndex = ${commandsArrIndex}`)
-                if (commandsArr[commandsArrIndex] == commandsArr[commandsArrIndex.length-1])
+                if (commandsArrIndex == (commandsArrIndex.length-1))
                     message.delete().catch(err => console.log(err))
             }
             else if (command=='my' && (args[0]=='orders' || args[0]=='order')) {
