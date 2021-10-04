e635c8c445f697fc039a30a9a8a281025cc6ce5d
MrSofty123 <shaheer1642@gmail.com>
2021-10-05 03:12:54 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -157,8 +157,9 @@ client.on('messageCreate', async message => {
         }
         if (!order_data.messages_log)
             order_data.messages_log = []
-        order_data.messages_log = JSON.parse(order_data.messages_log)
-        order_data.messages_log.push(`<@${message.author.id}>: ${message.content}`)
+        order_data.messages_log = JSON.parse(order_data.messages_log).catch(err => console.log(err))
+        order_data.messages_log.push({author: message.author.id, content: message.content})
+        console.log()
         var status = await db.query(`
         UPDATE filled_users_orders
         SET messages_log = '${JSON.stringify(order_data.messages_log)}'
