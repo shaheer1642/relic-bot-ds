faa28a955b702ddd7d4e4ab11c0e95b4f0122ce6
MrSofty123 <shaheer1642@gmail.com>
2021-10-04 23:06:56 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -938,7 +938,7 @@ client.on('messageReactionAdd', async (reaction, user) => {
                 JOIN users_list ON users_orders.discord_id = users_list.discord_id
                 JOIN items_list ON users_orders.item_id = items_list.id
                 WHERE messages_ids.message_id = ${reaction.message.id} AND users_orders.visibility = true AND users_orders.order_type = '${order_type}'
-                ORDER BY users_list.ingame_name ASC`)
+                ORDER BY users_list.ingame_name`)
                 .then(res => {
                     if (res.rows.length == 0) {
                         reaction.message.channel.send(`⚠️ <@${tradee.discord_id}> That order no longer exists in the db. Please try another offer ⚠️`).then(msg => setTimeout(() => msg.delete().catch(err => console.log(err)), 10000)).catch(err => console.log(err));
