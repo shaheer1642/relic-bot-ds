f763d0e71c853a00de54a63099f7343739506b78
MrSofty123 <shaheer1642@gmail.com>
2021-10-06 03:08:34 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -184,7 +184,14 @@ client.on('messageCreate', async message => {
             return Promise.resolve()
         //order_data.messages_log = JSON.parse(order_data.messages_log)
         var sentMessage = ''
-        sentMessage += message.content.replace(/'/g,`''`) + '\n'
+        sentMessage += message.content.replace(/\\n/g, "\\n")
+        .replace(/\\'/g, "\\'")
+        .replace(/\\"/g, '\\"')
+        .replace(/\\&/g, "\\&")
+        .replace(/\\r/g, "\\r")
+        .replace(/\\t/g, "\\t")
+        .replace(/\\b/g, "\\b")
+        .replace(/\\f/g, "\\f") + '\n'
         message.attachments.map(attachment => {
             sentMessage += attachment.url + '\n'
         })
