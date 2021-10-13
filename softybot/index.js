41976c6c7fcbe09492067ad0d9eb975e3b3bca18
MrSofty123 <shaheer1642@gmail.com>
2021-10-13 20:33:24 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -5595,6 +5595,7 @@ async function trading_bot_item_orders(message,args,request_type = 1) {
     JOIN items_list ON users_orders.item_id=items_list.id 
     JOIN users_list ON users_orders.discord_id=users_list.discord_id 
     WHERE users_orders.item_id = '${item_id}' AND users_orders.order_type = '${order_type}'
+    ORDER BY users_orders.update_timestamp
     `)
     .then(res => {
         if (res.rows.length == 0) {
