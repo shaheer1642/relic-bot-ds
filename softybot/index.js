0a4ef74e79232f755aceba63f84da145f5961383
MrSofty123 <shaheer1642@gmail.com>
2021-10-20 19:28:31 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -4915,6 +4915,7 @@ async function updateDatabasePrices(up_origin) {
     .then(async (db_items_list) => {
         for (var i=0;i<db_items_list.rows.length;i++) {
             const item = db_items_list.rows[i]
+            console.log('checking ' + item.item_url)
             if (item.tags.includes("prime") || item.tags.includes("relic") || (item.tags.includes("mod") && item.tags.includes("legendary"))) {
                 var status = await updateDatabaseItem(db_items_list,item)
                 .then(() => {
