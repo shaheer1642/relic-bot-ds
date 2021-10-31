85c378c9507c795382c9cc907381b25e3f9ff5d3
MrSofty123 <shaheer1642@gmail.com>
2021-10-31 16:50:24 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -3361,7 +3361,7 @@ async function relics(message,args) {
         relics_timestamps = relics_timestamps.sort(dynamicSortDesc("vault_timestamp"))
         console.log(JSON.stringify(relics_timestamps))
         if (relics_timestamps.length >= 1)
-            postdata[X].embeds[j].footer.text = "Best Relic: " + relics_timestamps[0].link.replace(/_/g, " ").replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())
+            postdata[X].embeds[j].footer.text = "Best Relic: " + relics_timestamps[0].link.replace(/_/g, " ").replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase()).replace(" Relic",'')
         i++
         j++
     }
