85e74e9bc3d18adee6919e168eb0b864bce8ca04
MrSofty123 <shaheer1642@gmail.com>
2021-11-18 23:15:33 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -1604,8 +1604,8 @@ client.on('interactionCreate', async interaction => {
                     title: 'Prime sets >= ' + interaction.options.getNumber('threshold'),
                     description: 'something',
                     fields: [
-                        {name: 'Set', value: res.rows.map(e => {return e.item_url}).toString().replace(/,/g, '\u0009\u0009\n').replace(/_/g, " ").replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase()), inline: true},
-                        {name: 'Price', value: res.rows.map(e => {return e.sell_price}).toString().replace(/,/g, '\u0009\u0009\n'), inline: true},
+                        {name: 'Set', value: res.rows.map(e => {return e.item_url}).toString().replace(/,/g, '\u205F\u205F\u205F\u205F\u205F\n').replace(/_/g, " ").replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase()), inline: true},
+                        {name: 'Price', value: res.rows.map(e => {return e.sell_price}).toString().replace(/,/g, '\u205F\u205F\u205F\u205F\u205F\n'), inline: true},
                         {name: 'Ducat', value: res.rows.map(e => {return e.ducat}).toString().replace(/,/g, '\n'), inline: true}
                     ],
                     timestamp: new Date()
