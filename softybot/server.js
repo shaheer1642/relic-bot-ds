c3f1eec358a5b4dc7abbb8b84c6596af509578e6
MrSofty123 <shaheer1642@gmail.com>
2021-11-19 16:20:40 +0500

    changes

--- softybot/server.js
+++ softybot/server.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(express.static("express"));
 // default URL for website
-app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/website.html'));
+app.use('/', function(req,res) {
+    res.sendFile(path.join(__dirname+'/express/index.html'));
     //__dirname : It will resolve to your project folder.
   });
 const server = http.createServer(app);
