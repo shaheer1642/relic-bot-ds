750c48352b882bf376e07684489849b53bbdc5cf
MrSofty123 <shaheer1642@gmail.com>
2021-11-19 16:20:57 +0500

    Update server.js

--- softybot/server.js
+++ softybot/server.js
@@ -10,6 +10,6 @@ app.use('/', function(req,res) {
     //__dirname : It will resolve to your project folder.
   });
 const server = http.createServer(app);
-const port = 3000;
+const port = process.env.PORT;
 server.listen(port);
 console.debug('Server listening on port ' + port);
