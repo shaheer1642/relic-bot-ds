0c757b4bbf29f2d71cdef6a8f771591c94f6f08c
MrSofty123 <shaheer1642@gmail.com>
2021-11-21 18:39:23 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -5165,17 +5165,19 @@ async function gpt3_reset(message,args) {
 async function gpt3_completion(message,args) {
     console.log(args)
     // The new question asked by the user.
-    gpt3chatLog += '\nHuman: ' + args.toString().replace(/,/g, " ");
+    gpt3chatLog += '\nQ: ' + args.toString().replace(/,/g, " ");
     console.log(gpt3chatLog)
     
     const url = 'https://api.openai.com/v1/engines/davinci/completions';
     const params = {
-        'prompt': gpt3chatLog,
-        'max_tokens': 150,
-        'temperature': 0.9,
-        'frequency_penalty': 0,
-        'presence_penalty': 0.6,
-        'stop': '\nHuman'
+        engine: "davinci",
+        prompt: "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:",
+        temperature: 0,
+        max_tokens: 100,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+        stop: ["\n"]
     };
     const headers = {
         'Authorization': `Bearer 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`,
