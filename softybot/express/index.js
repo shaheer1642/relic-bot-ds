b2110600fe862793e18f1beb1ebb985f51374246
MrSofty123 <shaheer1642@gmail.com>
2021-12-02 13:44:59 +0500

    Update index.js

--- softybot/express/index.js
+++ softybot/express/index.js
@@ -1,3 +1,9 @@
+var toggleColor = 1
+
 function changeColor() {
-    document.getElementById("demo").style.color = "red";
+    toggleColor = !toggleColor
+    if (toggleColor)
+        document.getElementById("demo").style.color = "red";
+    else
+        document.getElementById("demo").style.color = "black";
 }
