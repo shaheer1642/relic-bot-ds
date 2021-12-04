a9f9cda3a2a4808260487f3186290c57ae67c681
MrSofty123 <shaheer1642@gmail.com>
2021-12-04 15:10:43 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -9971,8 +9971,8 @@ async function bounty_check() {
                             fields: [
                                 {name: 'Syndicate', value: syndicate.syndicate, inline: true},
                                 {name: 'Mission', value: job.type, inline: true},
-                                {name: 'Level', value: job.enemyLevels.join('-'), inline: true},
                                 {name: 'Rewards', value: job.rewardPool.join('\n'), inline: false},
+                                {name: 'Level', value: job.enemyLevels.join('-'), inline: true},
                                 {name: 'Expires', value: `<t:${Math.round(new Date(syndicate.expiry).getTime()/1000)}:R> (<t:${Math.round(new Date(syndicate.expiry).getTime()/1000)}:f>)`, inline: false}
                             ],
                             color: bountyDB.color
