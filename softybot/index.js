77aa2190199a448b6492858077a6ed3186c9cc93
MrSofty123 <shaheer1642@gmail.com>
2021-12-05 17:10:21 +0500

    changes

--- softybot/index.js
+++ softybot/index.js
@@ -8687,11 +8687,11 @@ async function trading_lich_bot(interaction) {
     //----------------
 
     if (!interaction.member.presence) {
-        interaction.reply({content: `⚠️ Your discord status must be online to use the bot. Use the command ``my orders`` in <#892003772698611723> to post your lich order ⚠️`, ephemeral: true}).catch(err => console.log(err))
+        interaction.reply({content: `⚠️ Your discord status must be online to use the bot. Use the command \`my orders\` in <#892003772698611723> to post your lich order ⚠️`, ephemeral: true}).catch(err => console.log(err))
         return Promise.resolve()
     }
     if (interaction.member.presence.status == `offline`) {
-        interaction.reply({content: `⚠️ Your discord status must be online to use the bot. Use the command ``my orders`` in <#892003772698611723> to post your lich order  ⚠️`, ephemeral: true}).catch(err => console.log(err))
+        interaction.reply({content: `⚠️ Your discord status must be online to use the bot. Use the command \`my orders\` in <#892003772698611723> to post your lich order  ⚠️`, ephemeral: true}).catch(err => console.log(err))
         return Promise.resolve()
         //test
     }
