a8694132ee0490b75f07cc24ab71cf90e85afcf9
MrSofty123 <shaheer1642@gmail.com>
2021-12-12 18:57:53 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -12,7 +12,7 @@ const axiosRetry = require('axios-retry');
 const wfm_api = require('./modules/wfm_api.js');
 const test_modules = require('./modules/test_modules.js');
 const trade_bot_modules = require('./modules/trade_bot_modules.js');
-const extras = require('./modules/extras.js');
+const {inform_dc,dynamicSort,dynamicSortDesc,msToTime,msToFullTime,mod_log} = require('./extras.js');
 const Canvas = require('canvas')
 const fs = require('fs')
 const {db} = require('./modules/db_connection.js');
@@ -6389,95 +6389,6 @@ async function dc_ducat_update() {
 }
 //-------------------------------------------
 
-function msToTime(s) {
-
-    // Pad to 2 or 3 digits, default is 2
-    function pad(n, z) {
-      z = z || 2;
-      return ('00' + n).slice(-z);
-    }
-  
-    var ms = s % 1000;
-    s = (s - ms) / 1000;
-    var secs = s % 60;
-    s = (s - secs) / 60;
-    var mins = s % 60;
-    var hrs = (s - mins) / 60;
-  
-    if (hrs != 0)
-        return pad(hrs) + ' hours ' + pad(mins) + ' minutes ' + pad(secs) + ' seconds';
-    if (mins != 0)
-        return pad(mins) + ' minutes ' + pad(secs) + ' seconds';
-    return pad(secs) + ' seconds';
-}
-
-function msToFullTime(ms) {
-    console.log(ms)
-    var seconds = Math.floor(ms / 1000),
-    minutes = Math.floor(seconds / 60),
-    hours   = Math.floor(minutes / 60),
-    days    = Math.floor(hours / 24),
-    months  = Math.floor(days / 30),
-    years   = Math.floor(days / 365);
-    seconds %= 60;
-    minutes %= 60;
-    hours %= 24;
-    days %= 30;
-    months %= 12;
-
-    var str = ''
-    if (years != 0)
-        if (years > 1)
-            str += years + ' years'
-        else
-            str += years + ' year'
-    if (months != 0)
-        if (months > 1)
-            str += ' ' + months + ' months'
-        else
-            str += ' ' + months + ' month'
-    if (days != 0)
-        if (days > 1)
-            str += ' ' + days + ' days'
-        else
-            str += ' ' + days + ' day'
-
-    if (str == '')
-        str = `${hours} hours ${minutes} minutes ${seconds} seconds`
-
-    return str;
-}
-
-function dynamicSort(property) {
-    var sortOrder = 1;
-    if(property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-    }
-    return function (a,b) {
-        /* next line works with strings and numbers, 
-         * and you may want to customize it to your needs
-         */
-        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-        return result * sortOrder;
-    }
-}
-
-function dynamicSortDesc(property) {
-    var sortOrder = 1;
-    if(property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-    }
-    return function (a,b) {
-        /* next line works with strings and numbers, 
-         * and you may want to customize it to your needs
-         */
-        var result = (a[property] > b[property]) ? -1 : (a[property] < b[property]) ? 1 : 0;
-        return result * sortOrder;
-    }
-}
-
 function trades_update() {
     console.log('Executing trades_update')
     let relicsArr = []
@@ -7530,17 +7441,6 @@ async function backupItemsList() {
     }).catch(err => console.log(err))
 }
 */
-async function inform_dc (str) {
-    await client.channels.cache.get('891756819045826621').send(str).catch(err => console.log(err+'\nError posting bot update.'))
-}
-
-async function mod_log (str,color='RANDOM') {
-    const embed = new MessageEmbed()
-        .setColor(color)
-        .setDescription(str)
-        .setTimestamp()
-    await client.channels.cache.get('892072612002418718').send({content: " ", embeds: [embed]}).catch(err => console.log(err+'\nError posting moderation update.'))
-}
 
 axiosRetry(axios, {
     retries: 50, // number of retries
