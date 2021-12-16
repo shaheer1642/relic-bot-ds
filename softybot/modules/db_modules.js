158f6a562f7260ad7d0ae3526823442466a0c377
MrSofty123 <shaheer1642@gmail.com>
2021-12-16 08:57:15 +0500

    change

--- softybot/modules/db_modules.js
+++ softybot/modules/db_modules.js
@@ -508,6 +508,8 @@ async function updateDatabaseItem(db_items_list,item,index) {
                         var pos2 = str.indexOf('(',pos1)
                         pos1 = str.indexOf(')',pos2)
                         vault_timestamp = new Date(str.substring(pos2+1,pos1)).getTime()
+                        if (vault_timestamp == NaN)
+                            vault_timestamp = 0
                         console.log('Updating DB relic vault timestamp...')
                         var status = await db.query(`UPDATE items_list SET 
                             vault_timestamp = ${vault_timestamp}
