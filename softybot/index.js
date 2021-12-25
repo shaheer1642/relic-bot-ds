10e90794c8b0b1eb51f3a2b3ca7afd9a4356ca2f
MrSofty123 <shaheer1642@gmail.com>
2021-12-26 00:09:29 +0500

    Update index.js

--- softybot/index.js
+++ softybot/index.js
@@ -1823,7 +1823,7 @@ client.on('interactionCreate', async interaction => {
                         break
                     var bounty = bounties_list[i]
                     if (bounty.type.toLowerCase().match(mission_type.toLowerCase())) {
-                        if (bounty.users && bounty.users.match(interaction.member.id))
+                        if (bounty.user2.hasOwnProperty(interaction.member.id))
                             postdata.push({name: bounty.type + ' (Remove)', value: bounty.type.toLowerCase().replace(/ /g,'_')})
                         else
                             postdata.push({name: bounty.type, value: bounty.type.toLowerCase().replace(/ /g,'_')})
