397a9c55ebf20f097c8c9e0611852e59bc2217f5
MrSofty123 <shaheer1642@gmail.com>
2021-12-28 23:02:15 +0500

    Update testjs.js

--- softybot/testjs.js
+++ softybot/testjs.js
@@ -1,16 +1,23 @@
-var postdata = {embeds:[]}
+const {db} = require('./modules/db_connection.js')
+const axios = require('axios');
 
-var ind = postdata.embeds.push({
-    title: 'title',
-    url: "https://warframe.market/items/",
-    footer: {text: "Total drops value: p"},
-    thumbnail: {url: 'https://warframe.market/static/assets/'},
-    fields: [
-        {name: "`Drops`", value: 12, inline: true},
-        {name: "`Price`", value: 13, inline: true},
-        {name: "`Ducat`", value: 13, inline: true}]
+axios("https://api.warframe.market/v1/items")
+.then(res => {
+    const json = res.data
+    json.payload2 = json.payload
+    json.payload3 = json.payload
+    json.payload4 = json.payload
+    json.payload5 = json.payload
+    json.payload6 = json.payload
+    console.log(json)
+    db.query(`insert into untitled_table_194 (test) VALUES ('${JSON.stringify(json).replace(/'/g,`''`)}')`)
+    .then(res => {
+        console.log('success')
+    })
+    .catch(err => {
+        console.log(err)
+    })
 })
-
-postdata.embeds[ind-1].description = 'desc'
-
-console.log(postdata)
+.catch(err => {
+    console.log(err)
+})
