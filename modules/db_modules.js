d8a1ad732155d7e2becc478aca6b0d0cc16d025c
MrSofty123 <shaheer1642@gmail.com>
2022-01-08 22:08:23 +0500

    Update db_modules.js

--- modules/db_modules.js
+++ modules/db_modules.js
@@ -702,12 +702,7 @@ async function getDB(message,args) {
             message.channel.send(`Some error occured compiling 'users_list'. Please contact MrSofty#7926`).catch(err => console.log(err))
             return
         }
-        var status = await db.query(`
-        SELECT * FROM users_orders 
-        JOIN items_list ON users_orders.item_id=items_list.id 
-        JOIN users_list ON users_orders.discord_id=users_list.discord_id 
-        ORDER BY users_orders.visibility DESC
-        `)
+        var status = await db.query(`select * from users_list`)
         .then(res => {
             if (res.rows.length == 0)
                 return false
