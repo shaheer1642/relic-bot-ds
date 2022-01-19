af868f317f89ba19adf7692231fd9c0504219997
MrSofty123 <shaheer1642@gmail.com>
2022-01-20 01:49:00 +0500

    Update osiris.js

--- modules/osiris.js
+++ modules/osiris.js
@@ -12,7 +12,7 @@ async function sendMsg() {
         guild.emojis.fetch()
         .then(async emoji => {
             var emote_list = []
-            emoji.map(emote => emote_list.push(emote.identifier.match(/^a:[a-zA-Z0-9!@#$%^&*()_+-=]*:/)?  '<' + emote.identifier + '>':'<:' + emote.identifier + '>'))
+            emoji.map(emote => emote_list.push(emote.identifier))
             await db.query(`SELECT * from osiris_emotes`)
             .then(async res => {
                 for (const emote of emote_list) {
@@ -66,7 +66,7 @@ List of features:
                     })
                     x += 3
                 }
-                postdata.embeds[0].fields[x].value += emote + '\n'
+                postdata.embeds[0].fields[x].value += emote.match(/^a:[a-zA-Z0-9!@#$%^&*()_+-=]*:/)?  '<' + emote + '>'+ '\n':'<:' + emote + '>'+ '\n'
             }
             //channel.send(postdata).catch(err => console.log(err))
             message.edit(postdata).catch(err => console.log(err))
