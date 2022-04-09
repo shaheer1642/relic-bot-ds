9d49b188fb8ed82bf09145715129e58713f4b707
MrSofty123 <shaheer1642@gmail.com>
2022-04-10 01:59:17 +0500

    Update worldstatealerts.js

--- modules/worldstatealerts.js
+++ modules/worldstatealerts.js
@@ -1013,12 +1013,12 @@ async function fissures_check() {
             }
 
             fissures_list.normal.forEach(fissure => {
-                embed1.fields[0].value += `${emotes[fissure.tier].string} ${fissure.tier}\n`
+                embed1.fields[0].value += `${emotes[fissure.tier]} ${fissure.tier}\n`
                 embed1.fields[1].value += `${fissure.missionType} - ${fissure.node}\n`
                 embed1.fields[2].value += `<t:${Math.round(new Date(fissure.expiry).getTime() / 1000)}:R>\n`
             })
             fissures_list.voidStorm.forEach(fissure => {
-                embed2.fields[0].value += `${emotes[fissure.tier].string} ${fissure.tier}\n`
+                embed2.fields[0].value += `${emotes[fissure.tier]} ${fissure.tier}\n`
                 embed2.fields[1].value += `${fissure.missionType} - ${fissure.node}\n`
                 embed2.fields[2].value += `<t:${Math.round(new Date(fissure.expiry).getTime() / 1000)}:R>\n`
             })
