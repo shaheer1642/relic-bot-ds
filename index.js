bef9a55a60fdae5c4ba0b424846c0d814001d4ef
MrSofty123 <90972275+MrSofty123@users.noreply.github.com>
2022-04-12 02:35:37 +0500

    changes

--- index.js
+++ index.js
@@ -3532,6 +3532,82 @@ client.on('messageReactionAdd', async (reaction, user) => {
         }
         else
             reaction.users.remove(user.id);
+        
+        // Create chart for mastery distribution
+        const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
+
+        const width = 300; //px
+        const height = 300; //px
+        const backgroundColour = 'transparent'; // Uses https://www.w3schools.com/tags/canvas_fillstyle.asp
+        const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height, backgroundColour, plugins: {
+            modern: ['chartjs-plugin-datalabels']
+        } });
+
+        var data_backgroundColor = []
+        var data = []
+
+        //get data from server
+        client.guilds.cache.get('776804537095684108').roles.cache.map(role => {
+            if (role.name == "MR8+" || role.name == "MR16+" || role.name == "MR20+" || role.name == "MR25+" || role.name == "MR30+") {
+                data_backgroundColor.push(role.color)
+                data.push(role.members.length)
+            }
+        })
+
+        const configuration = {
+            type: 'doughnut',
+            data: {
+            labels: ['8+', '16+', '20+', '25+', '30+'],
+            datasets: [{
+                data: data
+            }],
+            backgroundColor: data_backgroundColor
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    datalabels: {
+                    display: true,
+                    formatter: (val, ctx) => {
+                        return ctx.chart.data.labels[ctx.dataIndex];
+                    },
+                    color: '#fff',
+                    backgroundColor: '#404040'
+                    },
+                }
+            }
+        }
+
+        chartJSNodeCanvas.renderToDataURL(configuration).then(dataUrl => {
+            const base64Image = dataUrl
+            
+            client.channels.cache.get('891923650649939989').messages.fetch('892084165405716541').then(msg => {
+                msg.edit({
+                    content: ' ',
+                    embeds: [{
+                        description: `
+                            React to this message with desired emoji to obtain your mastery role.
+                            <:MR8:892062162376327198> <@&891984426693697537>
+                            <:MR16:892062164813225994> <@&891984653928525834>
+                            <:MR20:892062164389625898> <@&891985679804928000>
+                            <:MR25:892062165115224074> <@&891986636672487484>
+                            <:MR30:892062165501087765> <@&891986953145290772>
+                        `
+                    }]
+                }).catch(err => console.log(err))
+            })
+
+            var base64Data = base64Image.replace(/^data:image\/png;base64,/, "");
+            
+            
+            fs.writeFile("out.png", base64Data, 'base64', function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        })
     }
     if (reaction.message.id == otherRolesMessageId) {
         if (reaction.emoji.id == "957325143699501156") {
