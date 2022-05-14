b5ec15a859c7eef5291c6dc524b438fc1e0ed654
MrSofty123 <shaheer1642@gmail.com>
2022-05-14 20:35:27 +0500

    Update worldstatealerts.js

--- modules/worldstatealerts.js
+++ modules/worldstatealerts.js
@@ -1822,6 +1822,7 @@ async function fissures_check() {
             })
 
             var timer = min_expiry - new Date().getTime()
+            if (timer > 180000) timer -= 180000  // check 3 min before for reset
             fissuresTimer = setTimeout(fissures_check, timer)
             console.log('fissures_check invokes in ' + msToTime(timer))
         }).catch(err => console.log(err))
