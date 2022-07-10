62311a05aac6a317cdde6b8814d68af780b8acd0
MrSofty123 <shaheer1642@gmail.com>
2022-07-10 05:12:21 +0500

    Update botv_recruit.js

--- modules/botv_recruit.js
+++ modules/botv_recruit.js
@@ -3,6 +3,68 @@ const {client} = require('./discord_client.js');
 const {inform_dc,dynamicSort,dynamicSortDesc,msToTime,msToFullTime,embedScore} = require('./extras.js');
 
 function send_msg(msg, args) {
-    client.channels.cache.get('950400363410915348').send('empty').catch*(err => console.log(err))
+    client.channels.cache.get('950400363410915348').send({content: 'empty'}).catch*(err => console.log(err))
     //
+    
+}
+
+function edit_main_msg() {
+    client.channels.cache.get('950400363410915348').messages.cache.get('000').edit(
+        {
+            content: ' ',
+            embeds: [{
+                title: 'Recruitment',
+                description: 'empty'
+            }],
+            components: [
+                {
+                    type: 1,
+                    components: [
+                        {
+                            type: 2,
+                            label: "1",
+                            style: 1,
+                            custom_id: "tb_verify"
+                        },
+                        {
+                            type: 2,
+                            label: "2",
+                            style: 2,
+                            custom_id: "tb_my_profile"
+                        },
+                        {
+                            type: 2,
+                            label: "3",
+                            style: 3,
+                            custom_id: "tb_actv_orders"
+                        },
+                        {
+                            type: 2,
+                            label: "4",
+                            style: 4,
+                            custom_id: "tb_close_orders"
+                        },
+                        {
+                            type: 2,
+                            label: "5",
+                            style: 4,
+                            custom_id: "tb_close_orders"
+                        },
+                        {
+                            type: 2,
+                            label: "6",
+                            style: 4,
+                            custom_id: "tb_close_orders"
+                        }
+                    ]
+        
+                }
+            ]
+        }
+    ).catch(err => console.log(err))
+}
+
+module.exports = {
+    send_msg,
+    edit_main_msg
 }
