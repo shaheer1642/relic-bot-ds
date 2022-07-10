cf613ee339e3f4425959652ebde451716b6bc173
MrSofty123 <shaheer1642@gmail.com>
2022-07-10 06:26:40 +0500

    Update botv_recruit.js

--- modules/botv_recruit.js
+++ modules/botv_recruit.js
@@ -9,6 +9,81 @@ function send_msg(msg, args) {
 }
 
 async function edit_main_msg() {
+    var squads = {
+        sq_fissures: {
+            name: 'Fissures',
+            id: 'sq_fissures',
+            spots: 4,
+            filled: 0
+        },
+        sq_sortie: {
+            name: 'Sortie',
+            id: 'sq_sortie',
+            spots: 4,
+            filled: 0
+        },
+        sq_incursions: {
+            name: 'Incursions',
+            id: 'sq_incursions',
+            spots: 4,
+            filled: 0
+        },
+        sq_alerts: {
+            name: 'Alerts',
+            id: 'sq_alerts',
+            spots: 4,
+            filled: 0
+        },
+        sq_eidolons: {
+            name: 'Eidolons',
+            id: 'sq_eidolons',
+            spots: 4,
+            filled: 0
+        },
+        sq_taxi_help: {
+            name: 'Taxi/Help',
+            id: 'sq_taxi_help',
+            spots: 2,
+            filled: 0
+        },
+        sq_mining_fishing: {
+            name: 'Mining/Fishing',
+            id: 'sq_mining_fishing',
+            spots: 2,
+            filled: 0
+        },
+        sq_bounties: {
+            name: 'Bounties',
+            id: 'sq_bounties',
+            spots: 4,
+            filled: 0
+        },
+        sq_leveling: {
+            name: 'Leveling',
+            id: 'sq_leveling',
+            spots: 4,
+            filled: 0
+        },
+        sq_index: {
+            name: 'Index',
+            id: 'sq_index',
+            spots: 4,
+            filled: 0
+        },
+        sq_arbitration: {
+            name: 'Arbitration',
+            id: 'sq_arbitration',
+            spots: 4,
+            filled: 0
+        },
+        sq_nightwave: {
+            name: 'Nightwave',
+            id: 'sq_nightwave',
+            spots: 2,
+            filled: 0
+        },
+    }
+
     var msg = await client.channels.cache.get('950400363410915348').messages.fetch('995482866614009876')
     msg.edit({
             content: ' ',
@@ -19,42 +94,32 @@ async function edit_main_msg() {
             components: [
                 {
                     type: 1,
-                    components: [
-                        {
-                            type: 2,
-                            label: "1",
-                            style: 1,
-                            custom_id: "1"
-                        },
-                        {
-                            type: 2,
-                            label: "2",
-                            style: 2,
-                            custom_id: "2"
-                        },
-                        {
-                            type: 2,
-                            label: "3",
-                            style: 3,
-                            custom_id: "3"
-                        },
-                        {
-                            type: 2,
-                            label: "4",
-                            style: 4,
-                            custom_id: "4"
-                        },
-                        {
-                            type: 2,
-                            label: "5",
-                            style: 4,
-                            custom_id: "5"
-                        }
-                    ]
-        
+                    components: getComponents()
                 }
             ]
     }).catch(err => console.log(err))
+
+    var componentIndex = 0
+    function getComponents() {
+        var components = [];
+        const squadsArr = Object.keys(squads)
+        for (var index=0; index<Object.keys(squads).length; index++) {
+            const squad = squadsArr[index];
+            if (index == componentIndex) {
+                components.push({
+                    type: 2,
+                    label: `${squads[squad].filled}/${squads[squad].spots} ${squads[squad].name}`,
+                    style: 1,
+                    custom_id: squads[squad].id
+                })
+                componentIndex++;
+                if ((componentIndex+1 % 5) == 0) {
+                    break
+                }
+            }
+        }
+        return components;
+    }
 }
 
 module.exports = {
