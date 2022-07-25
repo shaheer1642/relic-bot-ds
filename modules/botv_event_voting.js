62935c097b66b350a600658106c58f4e0dda1e5c
MrSofty123 <shaheer1642@gmail.com>
2022-07-25 20:52:39 +0500

    changes

--- modules/botv_event_voting.js
+++ modules/botv_event_voting.js
@@ -1,22 +1,5 @@
 const {client} = require('./discord_client.js');
 
-function bot_initialize(reaction,user) {
-    client.channels.fetch('817828725701476403').catch(err=>console.log(err))
-    .then(channel => {
-        channel.messages.fetch().catch(err=>console.log(err))
-        .then(messages => {
-            messages.forEach(async msg => {
-                await msg.reactions.removeAll().catch(err => console.log(err))
-                await msg.react('1️⃣').catch(err => console.log(err))
-                await msg.react('2️⃣').catch(err => console.log(err))
-                await msg.react('3️⃣').catch(err => console.log(err))
-                await msg.react('4️⃣').catch(err => console.log(err))
-                await msg.react('5️⃣').catch(err => console.log(err))
-            })
-        })
-    })
-}
-
 async function message_handler(message) {
     await message.react('1️⃣').catch(err => console.log(err))
     await message.react('2️⃣').catch(err => console.log(err))
@@ -29,12 +12,11 @@ function reaction_handler(reaction,user) {
     reaction.message.fetch().catch(err => console.log(err))
     .then(message => {
         if (user.id == message.author.id)
-            reaction.remove()
+            reaction.remove().catch(err => console.log(err))
     })
 }
 
 module.exports = {
     message_handler,
-    reaction_handler,
-    bot_initialize
+    reaction_handler
 }
