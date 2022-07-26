6fd054ac6396a0c13c49c1bb8c4ee553391bbd3d
MrSofty123 <shaheer1642@gmail.com>
2022-07-26 22:20:40 +0500

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -45,8 +45,8 @@ async function bot_initialize() {
 			})
 			
 			rest.put(Routes.applicationGuildCommands(client.user.id, guildId), { body: all_commands })
-				.then(() => console.log(`Successfully registered application commands ${all_commands.map(command => name).toString()} for guild ${guildId}`))
-				.catch(console.error);
+				.then(() => console.log(`Successfully registered application commands ${all_commands.map(command => command.name).toString()} for guild ${guildId}`))
+				.catch(err => console.log(err));
 		})
 
 	} catch(e) {
