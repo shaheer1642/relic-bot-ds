99e16c354dded5205b1ad40b22a8d26efd3b0768
MrSofty123 <shaheer1642@gmail.com>
2022-07-27 00:33:56 +0500

    Update twitch_affiliate.js

--- modules/twitch_affiliate.js
+++ modules/twitch_affiliate.js
@@ -2,7 +2,6 @@ const {db} = require('./db_connection.js');
 const {client} = require('./discord_client.js');
 const {ClientCredentialsAuthProvider} = require('@twurple/auth')
 const {ApiClient} = require('@twurple/api');
-const { Routes } = require('discord-api-types');
 const { WebhookClient } = require('discord.js');
 
 const authProvider = new ClientCredentialsAuthProvider(process.env.twitch_clientId, process.env.twitch_clientSecret);
