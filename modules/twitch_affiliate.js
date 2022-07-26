d941727e7c21a29c80a50ae8e35c3bd6c0150db7
MrSofty123 <shaheer1642@gmail.com>
2022-07-26 23:58:06 +0500

    Update twitch_affiliate.js

--- modules/twitch_affiliate.js
+++ modules/twitch_affiliate.js
@@ -14,7 +14,7 @@ async function interaction_handler(interaction) {
             // check if authorized user
             if (authorized_userIds.includes(interaction.user.id)) {
                 // verify channel existence in db
-                ensureChannelExistence(interaction.channel.id).catch(err => console.log(err))
+                ensureChannelExistence(interaction.channel.id).catch(err => interaction.reply({content: `Sorry, some error occured.\n${JSON.stringify(err)}`}))
                 .then(() => {
                     if (interaction.options.getSubcommand() == 'add') {
                         interaction.reply({content: 'test_add'}).catch(err => console.log(err))
@@ -31,6 +31,22 @@ async function interaction_handler(interaction) {
 
 async function ensureChannelExistence(channelId) {
     return new Promise((resolve,reject) => {
+        db.query(`SELECT * FROM twitch_affiliate_channels where channel_id = ${channelId}`).catch(err => reject(err))
+        .then(res => {
+            if (res.rowCount == 1) resolve()
+            else if (res.rowCount == 0) {
+                client.channels.fetch(channelId).catch(err => reject(err))
+                .then(channel => {
+                    channel.createWebhook('Twitch Affiliates (WarframeHub)',{avatar: 'https://cdn.discordapp.com/attachments/864199722676125757/1001563100438331453/purple-twitch-logo-png-18.png'}).catch(err => reject(err))
+                    .then(webhook => {
+                        db.query(`INSERT INTO twitch_affiliate_channels (channel_id,webhook_url,time_added) VALUES (${channelId},'${webhook.url}',${new Date().getTime()})`).catch(err => reject(err))
+                        .then(res => resolve())
+                    })
+                })
+            } else {
+                reject('unexpected result querying db, contact developer withe error code 501')
+            }
+        })
         resolve('yes')
     })
 }
