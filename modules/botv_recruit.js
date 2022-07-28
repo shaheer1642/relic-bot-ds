afbc38284613cc967853e61210b4e04ab08c67c1
MrSofty123 <shaheer1642@gmail.com>
2022-07-29 01:17:45 +0500

    Update botv_recruit.js

--- modules/botv_recruit.js
+++ modules/botv_recruit.js
@@ -73,37 +73,40 @@ function interactionHandler(interaction) {
         return
     } else {
         if (interaction.customId == 'sq_custom') {
-            
-            const modal = new ModalBuilder()
-            .setCustomId('myModal')
-            .setTitle('My Modal');
-
-            // Add components to modal
-
-            // Create the text input components
-            const favoriteColorInput = new TextInputBuilder()
-                .setCustomId('favoriteColorInput')
-                // The label is the prompt the user sees for this input
-                .setLabel("What's your favorite color?")
-                // Short means only a single line of text
-                .setStyle(TextInputStyle.Short);
-
-            const hobbiesInput = new TextInputBuilder()
-                .setCustomId('hobbiesInput')
-                .setLabel("What's some of your favorite hobbies?")
-                // Paragraph means multiple lines of text.
-                .setStyle(TextInputStyle.Paragraph);
-
-            // An action row only holds one text input,
-            // so you need one action row per text input.
-            const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
-            const secondActionRow = new ActionRowBuilder().addComponents(hobbiesInput);
-
-            // Add inputs to the modal
-            modal.addComponents(firstActionRow, secondActionRow);
-
-            // Show the modal to the user
-            interaction.showModal(modal);
+            try {
+                const modal = new ModalBuilder()
+                .setCustomId('myModal')
+                .setTitle('My Modal');
+    
+                // Add components to modal
+    
+                // Create the text input components
+                const favoriteColorInput = new TextInputBuilder()
+                    .setCustomId('favoriteColorInput')
+                    // The label is the prompt the user sees for this input
+                    .setLabel("What's your favorite color?")
+                    // Short means only a single line of text
+                    .setStyle(TextInputStyle.Short);
+    
+                const hobbiesInput = new TextInputBuilder()
+                    .setCustomId('hobbiesInput')
+                    .setLabel("What's some of your favorite hobbies?")
+                    // Paragraph means multiple lines of text.
+                    .setStyle(TextInputStyle.Paragraph);
+    
+                // An action row only holds one text input,
+                // so you need one action row per text input.
+                const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
+                const secondActionRow = new ActionRowBuilder().addComponents(hobbiesInput);
+    
+                // Add inputs to the modal
+                modal.addComponents(firstActionRow, secondActionRow);
+    
+                // Show the modal to the user
+                interaction.showModal(modal);
+            } catch(e) {
+                console.log(e)
+            }
     /*
             interaction.show
             interaction.reply({
