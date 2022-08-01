9648f7315c4698bd08fe5b45bd441c7f4c487ab5
MrSofty123 <shaheer1642@gmail.com>
2022-08-02 01:05:40 +0500

    Update botv_event_voting.js

--- modules/botv_event_voting.js
+++ modules/botv_event_voting.js
@@ -38,34 +38,38 @@ async function reaction_handler(reaction, user, action) {
 }
 
 async function calculate_votes(message) {
-    var users = {}
-    client.channels.fetch('817828725701476403').catch(err => console.log(err))
-    .then(channel => {
-        channel.messages.fetch().catch(err => console.log(err))
-        .then(messages => {
-            messages.map(async (message) => {
-                if (!users[message.author.id])
-                    users[message.author.id] = {username: message.author.username, points: 0, displayName: await client.guilds.cache.get('776804537095684108').members.fetch(message.author.id).then(res => {return res.displayName})}
-                message.reactions.map(reaction => {
-                    if (reaction.name == '1️⃣') users[message.author.id].points += 1
-                    if (reaction.name == '2️⃣') users[message.author.id].points += 2
-                    if (reaction.name == '3️⃣') users[message.author.id].points += 3
-                    if (reaction.name == '4️⃣') users[message.author.id].points += 4
-                    if (reaction.name == '5️⃣') users[message.author.id].points += 5
+    try {
+        var users = {}
+        client.channels.fetch('817828725701476403').catch(err => console.log(err))
+        .then(channel => {
+            channel.messages.fetch().catch(err => console.log(err))
+            .then(messages => {
+                messages.map(async (message) => {
+                    if (!users[message.author.id])
+                        users[message.author.id] = {username: message.author.username, points: 0, displayName: await client.guilds.cache.get('776804537095684108').members.fetch(message.author.id).then(res => {return res.displayName})}
+                    message.reactions.cache.forEach(reaction => {
+                        if (reaction.emoji.name == '1️⃣') users[message.author.id].points += 1
+                        if (reaction.emoji.name == '2️⃣') users[message.author.id].points += 2
+                        if (reaction.emoji.name == '3️⃣') users[message.author.id].points += 3
+                        if (reaction.emoji.name == '4️⃣') users[message.author.id].points += 4
+                        if (reaction.emoji.name == '5️⃣') users[message.author.id].points += 5
+                    })
                 })
             })
+            var embeds = [{
+                description: ''
+            }]
+            for (const userId in users) {
+                embeds[0].description += `<@${userId} (${users[userId].displayName}): ${users[userId].points} points>\n`
+            }
+            message.channel.send({
+                content: ' ',
+                embeds: embeds
+            }).catch(err => console.log(err))
         })
-        var embeds = [{
-            description: ''
-        }]
-        for (const userId in users) {
-            embeds[0].description += `<@${userId} (${users[userId].displayName}): ${users[userId].points} points>\n`
-        }
-        message.channel.send({
-            content: ' ',
-            embeds: embeds
-        }).catch(err => console.log(err))
-    })
+    } catch (e) {
+        console.log(e)
+    }
 }
 
 module.exports = {
