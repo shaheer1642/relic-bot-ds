a6b1bbafcf411ec26b60523d8e0953127bdf2a5d
MrSofty123 <shaheer1642@gmail.com>
2022-08-02 06:48:31 +0500

    Update crud_server.js

--- modules/crud_server.js
+++ modules/crud_server.js
@@ -2,7 +2,23 @@ const {client} = require('./discord_client.js');
 
 function message_create(message) {
     var text = message.content.toLowerCase()
-    if (text.match(' ong ') || text.match(' ong. ') || text.match(' ong! ') || text.endsWith(' ong') || text.match(' ong.') || text.match(' ong!') || text == 'ong' || text == 'ong.' || text == 'ong?' || text == 'ong!') {
+    if (text.match(' ong ') || 
+    text.match(' ong. ') || 
+    text.match(' ong\' ') || 
+    text.match(' ongawd ') || 
+    text.match(' ong! ') || 
+    text.endsWith(' ong') || 
+    text.endsWith(' ongawd') || 
+    text.match(' ong.') || 
+    text.match(' ong!') || 
+    text == 'ong' || 
+    text == 'ong.' || 
+    text == 'ong?' || 
+    text == 'ong!'||
+    text == 'o n g'||
+    text == 'ong\''||
+    text == 'ongawd'
+    ) {
         setTimeout(() => {
             message.delete().catch(err => console.log(err))
         }, 500);
