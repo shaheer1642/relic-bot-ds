11d238902f1dda1f10e73342bf4799d050baf691
MrSofty123 <shaheer1642@gmail.com>
2022-09-15 04:00:12 +0500

    Update db_modules.js

--- modules/db_modules.js
+++ modules/db_modules.js
@@ -111,7 +111,7 @@ warframe_items_drops.forEach(drop => {
                 }
             }
         }
-        const item_url = drop.type.toLowerCase().trim().replace(/ /g,'_')
+        const item_url = drop.type.toLowerCase().trim().replace(/ /g,'_').replace('_chassis_blueprint','_chassis').replace('_systems_blueprint','_systems').replace('_neuroptics_blueprint','_neuroptics')
         const rarity = drop.chance == 0.2533 ? 'common':drop.chance == 0.11 ? 'uncommon':drop.chance == 0.02 ? 'rare':'unknown'
         if (rarity == 'unknown') {
             console.log('rarity could not be determined for drop',drop)
