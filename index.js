0b8f31a8fe1b5532bc4f9f653e8ffcd901e7034e
MrSofty123 <shaheer1642@gmail.com>
2022-11-11 22:31:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -144,12 +144,23 @@ client.on('messageCreate', async message => {
         return
     }
 
+
     if (message.author.id == hubapp.bot_id && Object.keys(hubapp.channel_ids).includes(message.channel.id))
         hubapp.message_create(message)
 
     //prevent botception
     if (message.author.bot)
         return Promise.resolve()
+
+    //auto-delete in warframe hub recruit channels
+    if (message.type == 'DEFAULT' && !message.author?.bot && (['1039266854545403935','901799370071101460','901799369941073940','901799369974616094','901799369647484979'].includes(message.channel.id))) {
+        const guild = client.guilds.cache.get(message.guild.id) || await client.guilds.fetch(message.guild.id).catch(console.error)
+        const member = guild.members.cache.get(message.author.id) || await guild.members.fetch(message.author.id).catch(console.error)
+        const adminRole = member.roles.cache.get('891717782348136488') || await member.roles.fetch('891717782348136488').catch(console.error)
+        const staffRole = member.roles.cache.get('891718083520122880') || await member.roles.fetch('891718083520122880').catch(console.error)
+        //const timeout = adminRole || staffRole ? 3600000 : 3000
+        setTimeout(() => message.delete().catch(console.error), timeout);
+    }
 
     if (message.guild) {
         if (message.guild.id==osiris_guild_id) {
