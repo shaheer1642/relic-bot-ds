7c586d7dd476ac29ddac797accb2b1a4b3d27d31
MrSofty123 <shaheer1642@gmail.com>
2022-11-13 16:13:25 +0500

    Create warframe_hub.js

---
+++ modules/warframe_hub.js
@@ -0,0 +1,41 @@
+const {client} = require('./modules/discord_client.js');
+const {db} = require('./modules/db_connection.js');
+
+const guild_id = '865904902941048862'
+const vip_channel_id = '1041306010331119667'
+const vip_message_id = '1041306046280499200'
+
+client.on('ready', () => {
+    edit_vip_message()
+})
+
+async function edit_vip_message() {
+    const channel = client.channels.cache.get(vip_channel_id) || await client.channels.fetch(vip_channel_id).catch(console.error)
+    if (!channel) return
+    const message = channel.messages.cache.get(vip_message_id) || await channel.messages.fetch(vip_message_id).catch(console.error)
+    if (!message) return
+    message.edit({
+        content: ' ',
+        embeds: [{
+            title: 'VIP Subscription',
+            description: 
+`Subscribe to monthly VIP access for only **$4.99** to gain premium perks including:
+
+◉ Removing ads from our (HubApp)[https://www.hubapp.site/]
+◉ Claiming the <@&1041308552905629746> role
+◉ relicbot_premium_features
+◉ tradebot_premium_features`
+        }],
+        components: [
+            {
+                type: 1,
+                components: [{
+                    type: 2,
+                    label: 'Purchase VIP',
+                    style: 2,
+                    custom_id: 'warframe_hub_purchase_vip'
+                }]
+            },
+        ]
+    }).catch(console.error)
+}
