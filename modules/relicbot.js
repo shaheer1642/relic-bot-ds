b185125f0157c22c62fac99f806d616ba1115248
MrSofty123 <shaheer1642@gmail.com>
2022-11-22 19:33:45 +0500

    Update relicbot.js

--- modules/relicbot.js
+++ modules/relicbot.js
@@ -26,7 +26,7 @@ client.on('messageCreate', async (message) => {
         setTimeout(() => message.delete().catch(console.error), 1000);
     }).catch((err) => {
         message.channel.send({content: err}).catch(console.error)
-        //.then(msg => setTimeout(() => {message.delete().catch(console.error);msg.delete().catch(console.error)}, 5000))
+        //.then(msg => setTimeout(() => {message.delete().catch(console.error);msg.delete().catch(console.error)}, 5000)) 
     })
 })
 
