07201aba312cd8c0cd2462cbd1d03e7f08217fd2
MrSofty123 <shaheer1642@gmail.com>
2022-12-10 01:17:41 +0500

    Update gmail_client.js

--- modules/gmail_client.js
+++ modules/gmail_client.js
@@ -168,7 +168,7 @@ async function gmail_api_call(auth) {
                         } else {
                             const guild = client.guilds.cache.get('865904902941048862') || await client.guilds.fetch('865904902941048862').catch(console.error)
                             const member = guild.members.cache.get(xx_discord) || await guild.members.fetch(xx_discord).catch(console.error)
-                            const role = guild.roles.cache.find(role => role.name.toLowerCase() === 'awaken')
+                            const role = guild.roles.cache.find(role => role.name.toLowerCase() === 'verified')
                             member.roles.add(role).catch(console.error)
                             if (res.rowCount == 1) {
                                 var status = await db.query(`UPDATE tradebot_users_list SET ingame_name='${temp[4]}' WHERE discord_id = ${xx_discord}`).then(res => {
