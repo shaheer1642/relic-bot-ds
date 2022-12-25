47a643397e6cac6496bf3d04d030430650e8d2a8
MrSofty123 <shaheer1642@gmail.com>
2022-12-25 05:07:49 +0500

    Update as_gabot.js

--- modules/as_gabot.js
+++ modules/as_gabot.js
@@ -292,7 +292,7 @@ db.on('notification', async (notification) => {
             //     })
             // }).catch(console.error)
             channel.send({
-                content: giveaway.winners_list.length == 0 ? `**Giveaway Ended: ${convertUpper(giveaway.item)}**\n*Not enough entries*` : `**Giveaway Ended**\n${giveaway.winners_list.map(id => `<@${id}>`).join(', ')} ${giveaway.winners_list.length > 1 ? 'have':'has'} won **${convertUpper(giveaway.item)}**`,
+                content: giveaway.winners_list.length == 0 ? `**Giveaway Ended: ${convertUpper(giveaway.item)}**\n*Not enough entries*` : `**Giveaway Ended**\n${giveaway.winners_list.map(id => `<@${id}>`).join(', ')} ${giveaway.winners_list.length > 1 ? 'have':'has'} won **${convertUpper(giveaway.item)}** giveaway by <@${giveaway.discord_id}>`,
             }).catch(console.error)
         } else if (giveaway.status == 'active') {
             message.edit(embedGenerator(giveaway)).catch(console.error)
