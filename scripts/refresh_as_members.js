357bb29ff90a9b89a0e36a0f7382cb613ce107cf
MrSofty123 <shaheer1642@gmail.com>
2022-12-26 23:01:39 +0500

    changes

--- scripts/refresh_as_members.js
+++ scripts/refresh_as_members.js
@@ -1,5 +1,5 @@
-const {client} = require('./modules/discord_client.js');
-const {db} = require('./modules/db_connection')
+const {client} = require('../modules/discord_client.js');
+const {db} = require('../modules/db_connection')
 const {WebhookClient} = require('discord.js')
 const uuid = require('uuid')
 
