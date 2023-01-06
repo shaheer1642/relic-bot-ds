021a175de4d19097f5fe197ff2d607acf0e03a6d
MrSofty123 <shaheer1642@gmail.com>
2023-01-06 21:35:49 +0500

    Update prime_resurgence_tracker.js

--- modules/prime_resurgence_tracker.js
+++ modules/prime_resurgence_tracker.js
@@ -4,7 +4,7 @@ const {client} = require('./discord_client.js');
 const items_list = ['axi_l4_relic','neo_v8_relic','meso_o3_relic','lith_o2_relic']
 
 setInterval(() => {
-    console.log('pr tracker invoked')
+    //console.log('pr tracker invoked')
     items_list.forEach(item_url => {
         axios(`https://api.warframe.market/v1/items/${item_url}/orders`)
         .then(async response => {
@@ -19,7 +19,7 @@ setInterval(() => {
 }, 10000);
 
 function sendAlert(order,item) {
-    console.log('sendAlert invoked')
+    //console.log('sendAlert invoked')
     client.channels.cache.get('892003813786017822').send({
         content: ' ',
         embeds: [{
