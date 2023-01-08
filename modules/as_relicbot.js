836cc7af65826115cb1b993e68b46aac7566f5aa
MrSofty123 <shaheer1642@gmail.com>
2023-01-09 03:51:14 +0500

    Update as_relicbot.js

--- modules/as_relicbot.js
+++ modules/as_relicbot.js
@@ -890,7 +890,7 @@ function constructTrackersEmbed(trackers, ephemeral) {
     }
     trackers.forEach(tracker => {
         if (payload.embeds[0].fields[1].value.length < 1000) {
-            payload.embeds[0].fields[0].value += `${emote_ids[tracker.tier]} ${convertUpper(tracker.tier)}` + '\n'
+            payload.embeds[0].fields[0].value += `${convertUpper(tracker.tier)}` + '\n'
             payload.embeds[0].fields[1].value += relicBotSquadToString(tracker) + '\n'
             payload.embeds[0].fields[2].value += (tracker.is_steelpath ? `${emote_ids.steel_essence} Steelpath` : tracker.is_railjack ? `${emote_ids.railjack} Railjack` : 'Normal') + '\n'
         }
