93c530514119a25b1e706325bc5d6e3b8110e2cc
MrSofty123 <shaheer1642@gmail.com>
2023-03-20 18:01:37 +0500

    Update msg_auto_delete.js

--- modules/msg_auto_delete.js
+++ modules/msg_auto_delete.js
@@ -11,26 +11,25 @@ client.on('ready', () => {
 })
 
 function db_schedule_msg_deletion(message_id, channel_id, delete_timeout) {
-    db.query(`INSERT INTO discord_msg_auto_delete (message_id, channel_id, delete_timestamp) VALUES ('${message_id}','${channel_id}',${new Date().getTime() + delete_timeout})`).catch(console.error)
+    db.query(`
+        INSERT INTO discord_msg_auto_delete (message_id, channel_id, delete_timestamp) 
+        VALUES ('${message_id}','${channel_id}',${new Date().getTime() + delete_timeout})
+    `).catch(console.error)
 }
 
 function schedule_deletion(db_obj) {
     setTimeout(async () => {
-        db.query(`DELETE FROM discord_msg_auto_delete WHERE id = ${db_obj.id}`).catch(console.error)
         const cnl = client.channels.cache.get(db_obj.channel_id) || await client.channels.fetch(db_obj.channel_id).catch(console.error)
         if (!cnl) return
         const msg = cnl.messages.cache.get(db_obj.message_id) || await cnl.messages.fetch(db_obj.message_id).catch(console.error)
         if (!msg) return
-        msg.delete().catch(console.error)
+        msg.delete().then(() => db.query(`DELETE FROM discord_msg_auto_delete WHERE id = ${db_obj.id}`).catch(console.error)).catch(console.error)
     }, db_obj.delete_timestamp - new Date().getTime());
 }
 
 db.on('notification', async (notification) => {
-    const payload = JSONbig.parse(notification.payload);
-    console.log('[warframe_hub] db notification: ',payload)
-
     if (notification.channel == 'discord_msg_auto_delete_insert') {
-        schedule_deletion(payload)
+        schedule_deletion(JSONbig.parse(notification.payload))
     }
 })
 
