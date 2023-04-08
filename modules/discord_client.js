7427f344b641a3198668d45d1165d67af098e9e8
MrSofty123 <shaheer1642@gmail.com>
2023-04-09 02:24:43 +0500

    cache sweeper

--- modules/discord_client.js
+++ modules/discord_client.js
@@ -1,8 +1,8 @@
 
 
-const {Client, Collection, Intents, MessageEmbed, MessageReaction, WebhookClient} = require('discord.js');
+const {Client, Options} = require('discord.js');
 
-const client = new Client({ intents: 14095, partials: ['REACTION', 'MESSAGE', 'CHANNEL', 'GUILD_MEMBER', 'USER']}) //{ intents: 14095 })
+const client = new Client({ intents: 14095, partials: ['REACTION', 'MESSAGE', 'CHANNEL', 'GUILD_MEMBER', 'USER'], sweepers: Options.defaultSweeperSettings}) //{ intents: 14095 })
 
 client.login(process.env.DISCORD_BOT_TOKEN).catch(err => {
     console.log(err);
