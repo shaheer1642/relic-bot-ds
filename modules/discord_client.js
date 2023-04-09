d8ff8e67a996cc4bb096af30f4d2bb819699b18b
MrSofty123 <shaheer1642@gmail.com>
2023-04-09 21:19:05 +0500

    Update discord_client.js

--- modules/discord_client.js
+++ modules/discord_client.js
@@ -11,10 +11,10 @@ const client = new Client({
 			interval: 3600, // Every hour...
 			lifetime: 1800,	// Remove messages older than 30 minutes.
 		},
-		users: {
-			interval: 3600, // Every hour...
-			filter: user => user?.bot && user?.id !== client.user.id, // Remove all bots.
-		},
+		// users: {
+		// 	interval: 3600, // Every hour...
+		// 	filter: user => user?.bot && user?.id !== client.user.id, // Remove all bots.
+		// },
     }
 })
 
