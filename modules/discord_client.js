e672d8a662ff4b025a2d1f7c0711586789c48b73
MrSofty123 <shaheer1642@gmail.com>
2023-04-09 16:49:36 +0500

    cache sweeper #2

--- modules/discord_client.js
+++ modules/discord_client.js
@@ -2,7 +2,21 @@
 
 const {Client, Options} = require('discord.js');
 
-const client = new Client({ intents: 14095, partials: ['REACTION', 'MESSAGE', 'CHANNEL', 'GUILD_MEMBER', 'USER'], sweepers: Options.defaultSweeperSettings}) //{ intents: 14095 })
+const client = new Client({ 
+    intents: 14095, 
+    partials: ['REACTION', 'MESSAGE', 'CHANNEL', 'GUILD_MEMBER', 'USER'], 
+    sweepers: {
+        ...Options.defaultSweeperSettings,
+		messages: {
+			interval: 3600, // Every hour...
+			lifetime: 1800,	// Remove messages older than 30 minutes.
+		},
+		users: {
+			interval: 3600, // Every hour...
+			filter: user => user.bot && user.id !== client.user.id, // Remove all bots.
+		},
+    }
+})
 
 client.login(process.env.DISCORD_BOT_TOKEN).catch(err => {
     console.log(err);
