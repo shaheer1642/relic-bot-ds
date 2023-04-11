c1edceaf0924e9cbaab19ad8083579b956d9411d
MrSofty123 <shaheer1642@gmail.com>
2023-04-12 00:38:07 +0500

    Update as_events.js

--- modules/as_events.js
+++ modules/as_events.js
@@ -1,6 +1,7 @@
 const {client} = require('./discord_client.js');
 const {db} = require('./db_connection.js');
 const { df_send_message } = require('./discord_functions.js');
+const { db_schedule_msg_deletion } = require('./msg_auto_delete.js');
 
 const channel_ids = {
     moderator: '909247527201689680',
@@ -18,7 +19,16 @@ client.on('messageCreate',(message) => {
                     description: `${message.content}${screenshots ? `\n\n**Attached Screenshots**\n${screenshots}` : ''}`,
                     color: '#7243c4'
                 }]
-            }, channel_ids.moderator).catch(console.error)
+            }, channel_ids.moderator).then((res) => {
+                message.channel.send({content: `<@${message.author.id}> Your submission has been forwarded. Thank you!`}).then(res => {
+                    db_schedule_msg_deletion(res.id,res.channel.id,15000)
+                }).catch(console.error)
+            }).catch((err) => {
+                message.channel.send({content: `<@${message.author.id}> Some error occured forwarding your submission. Please try again`}).then(res => {
+                    db_schedule_msg_deletion(res.id,res.channel.id,15000)
+                }).catch(console.error)
+            })
+            db_schedule_msg_deletion(message.id,message.channel.id,2000)
         }
     }
 })
