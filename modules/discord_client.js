6a35af9ebfdfabd8f39eea8132d9333876964ba1
shaheer1642 <shaheer1642@gmail.com>
2024-02-26 20:29:20 +0500

    Update discord_client.js

--- modules/discord_client.js
+++ modules/discord_client.js
@@ -1,13 +1,21 @@
 
 
-const {Client, Options} = require('discord.js');
+const { Client, Options } = require('discord.js');
 
-const client = new Client({ 
+const client = new Client({
 	restRequestTimeout: 60000,
-    intents: 14095, 
-    partials: ['REACTION', 'MESSAGE', 'CHANNEL', 'GUILD_MEMBER', 'USER'], 
-    sweepers: {
-        ...Options.defaultSweeperSettings,
+	intents: 14095,
+	partials: ['REACTION', 'MESSAGE', 'CHANNEL', 'GUILD_MEMBER', 'USER'],
+	sweepers: {
+		...Options.DefaultSweeperSettings,
+		messages: {
+			interval: 3_600, // Every hour.
+			lifetime: 1_800, // Remove messages older than 30 minutes.
+		},
+		users: {
+			interval: 3_600, // Every hour.
+			filter: () => user => user.bot && user.id !== user.client.user.id, // Remove all bots.
+		},
 		// messages: {
 		// 	interval: 3600, // Every hour...
 		// 	lifetime: 1800,	// Remove messages older than 30 minutes.
@@ -16,14 +24,14 @@ const client = new Client({
 		// // 	interval: 3600, // Every hour...
 		// // 	filter: user => user?.bot && user?.id !== client.user.id, // Remove all bots.
 		// // },
-    }
+	}
 })
 
 client.login(process.env.DISCORD_BOT_TOKEN).catch(err => {
-    console.log(err);
-    process.exit()
+	console.log(err);
+	process.exit()
 });
 
 const tickcount = new Date().getTime();
 
-module.exports = {client,tickcount};
+module.exports = { client, tickcount };
