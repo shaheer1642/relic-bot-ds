1de48c05ac8d13e16ca17bd8d1996a0dec13e3ec
cog012 <coldspine233@gmail.com>
2024-08-26 00:13:35 +0800

    Update index.js

--- gauss-prime-2.0/api/index.js
+++ gauss-prime-2.0/api/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use('/api', require('./routes/wfm'))
 
