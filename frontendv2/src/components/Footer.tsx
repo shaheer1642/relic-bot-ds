119a8600ec7769bcb99b915c708ac1d63b3bcb77
shaheer1642 <shaheer1642@gmail.com>
2025-03-31 23:15:33 +0500

    rewrite: Footer rewrite

--- frontendv2/src/components/Footer.tsx
+++ frontendv2/src/components/Footer.tsx
@@ -1,87 +1,109 @@
 /* eslint eqeqeq: "off", no-unused-vars: "off" */
-import React from 'react';
 import { Typography, Grid, Link } from '@mui/material';
-import { withHooksHOC } from '../withHooksHOC';
-import theme from '../theme';
+import { useTheme } from '@mui/material/styles';
 
-class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-
-    this.special_thanks_members = [
-      'Crive33', '--FL--MattHarrigan', 'tia1331', '-Isana_Yashiro-', 'JohnWickPrime', '--DR--XIII.XI.MMVIII', 'Ady88', 'Leopard33'
-    ]
-  }
-
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-  }
-
-  render() {
-    return (
-      <Grid container
-        sx={{ backgroundColor: 'primary.main', borderTop: `2px solid ${theme.palette.secondary.main}`, padding: '10px' }}
-        alignItems='center'
-        justifyContent='center'
-        rowSpacing={'10px'}
-        columnSpacing='20px'
-      >
-        {/* <Grid item xs='auto'>
-          <Typography variant='h4'>AllSquads</Typography>
-        </Grid>
-        <Grid item xs={12}></Grid> */}
-        <Grid item xs={'auto'}>
-          <Link style={{ cursor: 'pointer' }} color={'secondary.main'} onClick={() => { this.props.navigate('faq'); window.scrollTo({ top: 0, behavior: "smooth" }) }}>FAQ</Link>
-        </Grid>
-        <Grid item xs={'auto'}>|</Grid>
-        <Grid item xs={'auto'}>
-          <Link style={{ cursor: 'pointer' }} color={'secondary.main'} onClick={() => { this.props.navigate('terms-of-service'); window.scrollTo({ top: 0, behavior: "smooth" }) }}>Terms of Service</Link>
-        </Grid>
-        <Grid item xs={'auto'}>|</Grid>
-        <Grid item xs={'auto'}>
-          <Link style={{ cursor: 'pointer' }} color={'secondary.main'} onClick={() => { this.props.navigate('privacy-policy'); window.scrollTo({ top: 0, behavior: "smooth" }) }}>Privacy Policy</Link>
-        </Grid>
-        <Grid item xs={'auto'}>|</Grid>
-        <Grid item xs={'auto'}>
-          <Link style={{ cursor: 'pointer' }} color={'secondary.main'} onClick={() => { this.props.navigate('allsquads-review-2023'); window.scrollTo({ top: 0, behavior: "smooth" }) }}>AllSquads 2023 Review</Link>
-        </Grid>
-        <Grid item xs={12}></Grid>
-        <Grid item xs={'auto'} alignItems='center' display='flex'>
-          <img src="/icons/discord-icon.png" width={'32px'} height={'32px'} style={{ margin: '5px' }} />
-          <Link href="https://discord.gg/invite/346ZthxCe8" color={'secondary.main'}>Join us on Discord</Link>
-        </Grid>
-        <Grid item xs={'auto'}>|</Grid>
-        <Grid item xs={'auto'} alignItems='center' display='flex'>
-          <img src="/icons/patreon-icon.png" width={'24px'} height={'24px'} style={{ margin: '5px' }} />
-          <Link href="https://www.patreon.com/warframehub" color={'secondary.main'}>Support us on Patreon</Link>
-        </Grid>
-        <Grid item xs={12}></Grid>
-        <Grid item xs={12} justifyContent='center' display={'flex'}>
-          <Typography sx={{ wordWrap: 'break-word' }} color='secondary.dark'>
-            DISCLAIMER: Digital Extremes and Warframe are registered trademarks. This website has no direct affiliation with Digital Extremes. All recognizable artwork is intellectual property of these trademarks.
-          </Typography>
-        </Grid>
-        <Grid item xs={12} justifyContent='center' display={'flex'}>
-          <Typography sx={{ wordWrap: 'break-word' }} color='primary.light'>
-            {`Special thanks to the following members for helping development of this project: ${this.special_thanks_members.sort().join(', ')}`}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} justifyContent='center' display={'flex'}>
-          <Typography sx={{ wordWrap: 'break-word' }} color='primary.light'>
-            {`Sound effects are taken from `}
-            <Link href="https://pixabay.com/" target='_blank' color='primary.light'>Pixaby</Link>
-          </Typography>
-        </Grid>
-        <Grid item xs={12} justifyContent='center' display={'flex'}>
-          <Typography>© AllSquads 2022-23</Typography>
-        </Grid>
-      </Grid>
-    );
-  }
+interface FooterProps {
+  navigate: (path: string) => void;
 }
 
-export default withHooksHOC(Footer);
+const special_thanks_members = [
+  'Crive33', '--FL--MattHarrigan', 'tia1331', '-Isana_Yashiro-', 'JohnWickPrime', '--DR--XIII.XI.MMVIII', 'Ady88', 'Leopard33'
+];
+
+export default function Footer({ navigate }: FooterProps) {
+  const theme = useTheme();
+
+  const handleNavigation = (path: string) => {
+    navigate(path);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <Grid
+      container
+      sx={{
+        backgroundColor: 'primary.main',
+        borderTop: `2px solid ${theme.palette.secondary.main}`,
+        padding: '10px'
+      }}
+      alignItems='center'
+      justifyContent='center'
+      rowSpacing={1}
+      columnSpacing={2}
+    >
+      {/* <Grid xs='auto'>
+        <Typography variant='h4'>AllSquads</Typography>
+      </Grid>
+      <Grid size={12}></Grid> */}
+      <Grid>
+        <Link
+          sx={{ cursor: 'pointer' }}
+          color='secondary.main'
+          onClick={() => handleNavigation('faq')}
+        >
+          FAQ
+        </Link>
+      </Grid>
+      <Grid>|</Grid>
+      <Grid>
+        <Link
+          sx={{ cursor: 'pointer' }}
+          color='secondary.main'
+          onClick={() => handleNavigation('terms-of-service')}
+        >
+          Terms of Service
+        </Link>
+      </Grid>
+      <Grid>|</Grid>
+      <Grid>
+        <Link
+          sx={{ cursor: 'pointer' }}
+          color='secondary.main'
+          onClick={() => handleNavigation('privacy-policy')}
+        >
+          Privacy Policy
+        </Link>
+      </Grid>
+      <Grid>|</Grid>
+      <Grid>
+        <Link
+          sx={{ cursor: 'pointer' }}
+          color='secondary.main'
+          onClick={() => handleNavigation('allsquads-review-2023')}
+        >
+          AllSquads 2023 Review
+        </Link>
+      </Grid>
+      <Grid size={12} />
+      <Grid sx={{ display: 'flex', alignItems: 'center' }}>
+        <img src="/icons/discord-icon.png" width={32} height={32} style={{ margin: '5px' }} alt="Discord" />
+        <Link href="https://discord.gg/invite/346ZthxCe8" color='secondary.main'>Join us on Discord</Link>
+      </Grid>
+      <Grid>|</Grid>
+      <Grid sx={{ display: 'flex', alignItems: 'center' }}>
+        <img src="/icons/patreon-icon.png" width={24} height={24} style={{ margin: '5px' }} alt="Patreon" />
+        <Link href="https://www.patreon.com/warframehub" color='secondary.main'>Support us on Patreon</Link>
+      </Grid>
+      <Grid size={12} />
+      <Grid size={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Typography sx={{ wordWrap: 'break-word' }} color='secondary.dark'>
+          DISCLAIMER: Digital Extremes and Warframe are registered trademarks. This website has no direct affiliation with Digital Extremes. All recognizable artwork is intellectual property of these trademarks.
+        </Typography>
+      </Grid>
+      <Grid size={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Typography sx={{ wordWrap: 'break-word' }} color='primary.light'>
+          {`Special thanks to the following members for helping development of this project: ${special_thanks_members.sort().join(', ')}`}
+        </Typography>
+      </Grid>
+      <Grid size={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Typography sx={{ wordWrap: 'break-word' }} color='primary.light'>
+          {`Sound effects are taken from `}
+          <Link href="https://pixabay.com/" target='_blank' color='primary.light'>Pixaby</Link>
+        </Typography>
+      </Grid>
+      <Grid size={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Typography>© AllSquads 2022-23</Typography>
+      </Grid>
+    </Grid>
+  );
+};
